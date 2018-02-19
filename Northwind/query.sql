241c640060918b02ad2eb5d9c9c2b07742e82faf
efpr <efpr1997@gmail.com>
2018-02-19 12:38:03 +0000

    SQL Formater

--- Northwind/query.sql
+++ Northwind/query.sql
@@ -1,143 +1,209 @@
+/*
+	Titulo: Northwind DB
+
+	Autores: 	Eduardo Romão nº35477
+				João Dias nº 35476
+
+	Data:	19/02/2016	
+
+	Versão: Ver. 1.0 Rev. 01
+
+	Dialecto:Standard. Restado em Oracle*SQL ver.17.4
+*/
+
 -- 1.
-SELECT FIRSTNAME AS "NOME PRÓPIO", LASTNAME AS "APELIDO" FROM EMPLOYEES;
-
---3.
-SELECT TITLE AS "TÍTULO", FIRSTNAME AS "NOME PRÓPIO", LASTNAME AS "APELIDO" FROM EMPLOYEES
-ORDER BY TITLE ASC, LASTNAME DESC;
-
---5.
-SELECT FIRSTNAME AS "NOME PRÓPRIO", LASTNAME AS "APELIDO", 
-        COUNTRY AS "PAÍS" FROM EMPLOYEES
-WHERE COUNTRY!='USA';
-
---7.
-SELECT EMPLOYEE_ID AS "CÓDIGO DO EMPREGADO", ORDER_ID AS "CÓDIGO DA ENCOMENDA", 
-        CUSTOMER_ID AS "CÓDIGO DO CLIENTE", REQUIRED_DATE AS "DATA REQUERIDA",
-        SHIPPED_DATE AS "DATA DE ENVIO" FROM ORDERS
-WHERE REQUIRED_DATE<SHIPPED_DATE;
-
---9.
-SELECT FIRSTNAME AS "NOME PRÓPIO", LASTNAME AS "APELIDO" FROM EMPLOYEES
-WHERE LASTNAME BETWEEN 'J%' AND 'M%';
-
---11.
-select title_of_courtesy as "Título de Cortesia", firstname as "Nome Próprio",
-        lastname as "Apelido" from employees
-where title_of_courtesy like 'M%';
-
---12.
-select title_of_courtesy as "Título de Cortesia", firstname as "Nome Próprio",
-        lastname as "Apelido" from employees
-where title_of_courtesy like 'M_.';
-
---13.
-select title_of_courtesy as "Título de Cortesia", firstname as "Nome Próprio",
-        lastname as "Apelido" from employees
-where title_of_courtesy not like 'Ms.' and title_of_courtesy not like 'Mrs.';
-
---17. a.
-select order_id as "Código da encomenda", freight as "Custos dos Portes",
-        (freight*1.1) as "Custos dos Portes Acrescidos"
-from orders where freight>=500;
---    b.
-select order_id as "Código da encomenda", freight as "Custos dos Portes",
-        (freight*1.1) as "Total dos Portes"
-from orders where freight>=500;
-
---19.
-select sum(QUANTITY) as "Total de encomendas" from order_details where product_id =3;
-
---21.
-select round(avg(unit_price),3) as "Preço Médio" from order_details;
-
---22.
-select city, count(employee_id) from employees group by city;
-
---24.
-select "Empresa", "Total de Encomendas" from(
-
-select company_name as "Empresa", count(order_id) as "Total de Encomendas" from customers inner join orders on orders.customer_id = customers.customer_id
- group by company_name
- ) where "Total de Encomendas" > 15 order by "Total de Encomendas" DESC;
- 
-
+SELECT firstname AS "NOME PRÓPIO",
+       lastname AS "APELIDO"
+FROM employees;
 
 --2.
+SELECT contact_name AS nome,
+       company_name AS "empresa",
+       contact_title AS "title",
+       phone AS "telefone"
+FROM customers;
 
-select contact_name as nome, company_name as "empresa", contact_title as "title", phone as "telefone"
-from CUSTOMERS;
+--3.
+SELECT title AS "TÍTULO",
+       firstname AS "NOME PRÓPIO",
+       lastname AS "APELIDO"
+FROM employees
+ORDER BY title ASC,
+         lastname DESC;
 
 --4.
-select product_name, unit_price, quantity_per_unit
-from PRODUCTS
-where UNITS_IN_STOCK = 0;
+SELECT product_name,
+       unit_price,
+       quantity_per_unit
+FROM products
+WHERE units_in_stock = 0;
+
+--5.
+SELECT firstname AS "NOME PRÓPRIO",
+       lastname AS "APELIDO",
+       country AS "PAÍS"
+FROM employees
+WHERE country!='USA';
 
 --6.
-select order_date as "data encomenda", shipped_date as "data envio", customer_id as "id cliente", freight as "portes"
-from ORDERS
-where ORDER_DATE = '1997-05-19';
+SELECT order_date AS "data encomenda",
+       shipped_date AS "data envio",
+       customer_id AS "id cliente",
+       freight AS "portes"
+FROM orders
+WHERE order_date = '1997-05-19';
+
+--7.
+SELECT employee_id AS "CÓDIGO DO EMPREGADO",
+       order_id AS "CÓDIGO DA ENCOMENDA",
+       customer_id AS "CÓDIGO DO CLIENTE",
+       required_date AS "DATA REQUERIDA",
+       shipped_date AS "DATA DE ENVIO"
+FROM orders
+WHERE required_date<shipped_date;
 
 --8.
-select company_name as "nome empresa", contact_name as "nome contacto", fax as "numero fax"
-from CUSTOMERS
-where fax IS NOT NULL;
+SELECT company_name AS "nome empresa",
+       contact_name AS "nome contacto",
+       fax AS "numero fax"
+FROM customers
+WHERE fax IS NOT NULL;
+
+--9.
+SELECT firstname AS "NOME PRÓPIO",
+       lastname AS "APELIDO"
+FROM employees
+WHERE lastname BETWEEN 'J%' AND 'M%';
 
 --10.
-select title_of_courtesy as "Titulo de Cortesia", firstname as "Nome", lastname as "Apelido"
-from EMPLOYEES
-where title_of_courtesy = 'Mrs.' or title_of_courtesy = 'Ms.';
+SELECT title_of_courtesy AS "Titulo de Cortesia",
+       firstname AS "Nome",
+       lastname AS "Apelido"
+FROM employees
+WHERE title_of_courtesy = 'Mrs.'
+  OR title_of_courtesy = 'Ms.';
+
+--11.
+SELECT title_of_courtesy AS "Título de Cortesia",
+       firstname AS "Nome Próprio",
+       lastname AS "Apelido"
+FROM employees
+WHERE title_of_courtesy LIKE 'M%';
+
+--12.
+SELECT title_of_courtesy AS "Título de Cortesia",
+       firstname AS "Nome Próprio",
+       lastname AS "Apelido"
+FROM employees
+WHERE title_of_courtesy LIKE 'M_.';
+
+--13.
+SELECT title_of_courtesy AS "Título de Cortesia",
+       firstname AS "Nome Próprio",
+       lastname AS "Apelido"
+FROM employees
+WHERE title_of_courtesy NOT LIKE 'Ms.'
+  AND title_of_courtesy NOT LIKE 'Mrs.';
 
 --14.
-select firstname as "nome proprio", 
-        lastname as "apelido"
-from EMPLOYEES
-where title = 'Sales Representative' and (CITY = 'Seattle' or city = 'Redmond');
+SELECT firstname AS "nome proprio",
+       lastname AS "apelido"
+FROM employees
+WHERE title = 'Sales Representative'
+  AND (city = 'Seattle'
+       OR city = 'Redmond');
 
 --15.
-select firstname as "nome proprio", 
-        lastname as "apelido", city as "cidade", EMPLOYEE_ID as "id"
-from EMPLOYEES
-where title = 'Sales Representative' and (CITY = 'Seattle' or city = 'Redmond');
+SELECT firstname AS "nome proprio",
+       lastname AS "apelido",
+       city AS "cidade",
+       employee_id AS "id"
+FROM employees
+WHERE title = 'Sales Representative'
+  AND (city = 'Seattle'
+       OR city = 'Redmond');
 
 --16.
-select CONCAT(firstname, lastname) as "nome completo"
-from EMPLOYEES;
+SELECT concat(firstname, lastname) AS "nome completo"
+FROM employees;
+
+--17. a.
+SELECT order_id AS "Código da encomenda",
+       freight AS "Custos dos Portes",
+       (freight*1.1) AS "Custos dos Portes Acrescidos"
+FROM orders
+WHERE freight>=500;
+
+--    b.
+SELECT order_id AS "Código da encomenda",
+       freight AS "Custos dos Portes",
+       (freight*1.1) AS "Total dos Portes"
+FROM orders
+WHERE freight>=500;
 
 --18.
-select (firstname || ' ' || lastname || ' pode ser contactado em x' || extension) as "Informação de Contacto"
-from EMPLOYEES;
+SELECT (firstname || ' ' || lastname || ' pode ser contactado em x' || extension) AS "Informação de Contacto"
+FROM employees;
 
 --19.
-select sum(quantity) as "total de unidades"
-from ORDER_DETAILS
-where PRODUCT_ID = 3;
+SELECT sum(quantity) AS "Total de encomendas"
+FROM order_details
+WHERE product_id =3;
+
+--21.
+SELECT round(avg(unit_price),3) AS "Preço Médio"
+FROM order_details;
 
 --20.
-select PRODUCTS.PRODUCT_NAME as "Producto", sum(ORDER_DETAILS.QUANTITY) as "Unidades Totais"
-from ORDER_DETAILS inner join PRODUCTS on ORDER_DETAILS.PRODUCT_ID = PRODUCTS.PRODUCT_ID
-where ORDER_DETAILS.PRODUCT_ID = 3
-group by PRODUCTS.PRODUCT_NAME;
+SELECT products.product_name AS "Producto",
+       sum(order_details.quantity) AS "Unidades Totais"
+FROM order_details
+INNER JOIN products ON order_details.product_id = products.product_id
+WHERE order_details.product_id = 3
+GROUP BY products.product_name;
+
+--22.
+SELECT city,
+       count(employee_id)
+FROM employees
+GROUP BY city;
 
 --23.
-select city, "Numero de Representantes"
-from (
-    select city, count(employee_id) as "Numero de Representantes"
-    from EMPLOYEES
-    group by city)
-where "Numero de Representantes" > 1 
-order by "Numero de Representantes" ASC;
-
+SELECT city,
+       "Numero de Representantes"
+FROM
+  ( SELECT city,
+           count(employee_id) AS "Numero de Representantes"
+   FROM employees
+   GROUP BY city)
+WHERE "Numero de Representantes" > 1
+ORDER BY "Numero de Representantes" ASC;
 
 --24.
-select city, "Numero de Representantes"
-from (
-    select city, count(employee_id) as "Numero de Representantes"
-    from EMPLOYEES
-    where title = 'Sales Representative'
-    group by city)
-where "Numero de Representantes" > 1 ;
+SELECT city,
+       "Numero de Representantes"
+FROM
+  ( SELECT city,
+           count(employee_id) AS "Numero de Representantes"
+   FROM employees
+   WHERE title = 'Sales Representative'
+   GROUP BY city)
+WHERE "Numero de Representantes" > 1 ;
+
+--25.
+SELECT "Empresa",
+       "Total de Encomendas" 
+FROM
+  ( SELECT company_name AS "Empresa", 
+  		   count(order_id) AS "Total de Encomendas"
+   FROM customers
+   INNER JOIN orders ON orders.customer_id = customers.customer_id
+   GROUP BY company_name )
+WHERE "Total de Encomendas" > 15
+ORDER BY "Total de Encomendas" DESC;
 
 --26.
-select unit_price as "Preço Unitário",
-        '€' || CAST(unit_price as CHAR(10))
-from PRODUCTS;
+SELECT unit_price AS "Preço Unitário",
+       '€' || cast(unit_price AS char(10))
+FROM products;
+
